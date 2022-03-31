51427bb7c021ca2c8473d82956ca9b8da031cddb
cw <fromnowwon@gmail.com>
2022-03-31 17:40:33 +0900

    commit

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,14 @@ form.addEventListener('submit', e => {
 	console.log(data);
 })
 
-const person: any = {};
-console.log(person.speak());
+fetch('/api/idol/V89FC7J-ATJ44KV-PW8MZMA-7GED4E6/monsta-x')
+.then(res => res.json()) 
+.then(data => {
+	console.log(data)
+})
+.catch(error => {
+	console.log(error)
+}) 
+
+
+
